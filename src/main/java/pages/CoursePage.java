6cdb4fccc72da80b339b67691979e4eddff1b609
NatalyM <nbelokopytova@gmail.com>
2022-08-16 12:28:55 +0300

    добавлен upperCase в проверку названия курса

--- src/main/java/pages/CoursePage.java
+++ src/main/java/pages/CoursePage.java
@@ -15,12 +15,14 @@ public class CoursePage extends Page{
         By locator;
 
         //Локаторы для разных курсов различаются
-        if (!titleBeforeClick.contains("Специализация"))
-             locator = By.cssSelector(".course-header2__title");
+        if (!titleBeforeClick.toUpperCase().contains("СПЕЦИАЛИЗАЦИЯ"))
+            locator = By.cssSelector(".course-header2__title");
          else
              locator = By.tagName("title");
 
-       return driver.findElement(locator).getAttribute("innerText");
+        String result = driver.findElement(locator).getAttribute("innerText"); //Если эл-т не видим на странице, то getText() возвращает пустую строку, поэтому берем текст напрямую из свойств эл-та.
+
+        return result;
     }
 
 }
