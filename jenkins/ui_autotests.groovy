3e6360d6b34a5dd99557a3e8cc01c13716577202
NatalyM <nbelokopytova@gmail.com>
2022-12-13 12:35:45 +0300

    groovy

--- jenkins/ui_autotests.groovy
+++ jenkins/ui_autotests.groovy
@@ -20,8 +20,11 @@ timeout(180) {
         }
         stage('Telegram notify'){
             shell(
+                def bot_token = "5952779011:AAGHSuT02UfrsFarMFdsO8mKtD1SsqO2EO0"
+                def chat_id = "-1001844362560"
+
                 def message = "++++++++ UI tests report ++++++++\n"
-                message += "Build URL: ${env.JOB_URL}"
+                message += "Build URL: ${env.JOB_URL}\n"
                 message += "Report: \n"
 
                 def report = readFile "${env.WORKSPACE}/allure-results/export/influxDb.txt"
@@ -36,12 +39,14 @@ timeout(180) {
                     }
                 }
 
+                def url = "https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=${chat_id}&text=${message}"
+
                 if("${HTTP_PROXY}" != "") {
                     def proxyUrl = new URL("${HTTP_PROXY}")
                     def proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(),proxyUrl.getPort()))
-                    urlConnection = new URL(stringBuilder.toString()).openConnection(proxy) as HttpURLConnection
+                    urlConnection = new URL(url).openConnection(proxy) as HttpURLConnection
                 } else {
-                    urlConnection = new URL(stringBuilder.toString()).openConnection() as HttpURLConnection
+                    urlConnection = new URL(url).openConnection() as HttpURLConnection
                 }
 
                 urlConnection.setRequestMethod('GET')
