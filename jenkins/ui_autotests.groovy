a3cca1d4f7b643b64487c6c589001a2934559305
NatalyM <nbelokopytova@gmail.com>
2022-12-14 16:23:29 +0300

    TEST_allure_version

--- jenkins/ui_autotests.groovy
+++ jenkins/ui_autotests.groovy
@@ -20,7 +20,7 @@ timeout(60) {
                 currentBuild.result = 'UNSTABLE'
             }
             }
-//            stage('reports') {
+            stage('reports') {
 //                        allure([
 //                                includeProperties: false,
 //                                jdk: '',
@@ -28,45 +28,46 @@ timeout(60) {
 //                                reportBuildPolicy: 'ALWAYS',
 //                                results: [[path: 'allure-results']]
 //                        ])
-//            }
-            stage('Telegram notify') {
-                shell(
-                def bot_token = "5952779011:AAGHSuT02UfrsFarMFdsO8mKtD1SsqO2EO0"
-                def chat_id = "1001844362560"
-
-                def message = "++++++++ UI tests report ++++++++\n"
-                message += "Build URL: ${env.JOB_URL}\n"
-                message += "Report: \n"
-
-                def report = readFile "${env.WORKSPACE}/allure-results/export/influxDb.txt"
-                if (report =~ /.*Failed.*?\s+[1-9]+/) {
-                    status = " *FAILED*"
-                }
-
-                for (def status : ["Passed", "Failed", "Skipped"]) {
-                    def matcher = report =~ /.*\$\{status}.*=[1-9]+/
-                    if (matcher.find()) {
-                        message += "${status.toUpperCase()}: ${matcher[0][1]}"
-                    }
-                }
-
-                def url = "https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=-${chat_id}&text=${message}"
-
-                if ("${HTTP_PROXY}" != "") {
-                    def proxyUrl = new URL("${HTTP_PROXY}")
-                    def proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(), proxyUrl.getPort()))
-                    urlConnection = new URL(url).openConnection(proxy) as HttpURLConnection
-                } else {
-                    urlConnection = new URL(url).openConnection() as HttpURLConnection
-                }
-
-                urlConnection.setRequestMethod('GET')
-                urlConnection.setDoOutput(true)
-
-                def is = urlConnection.getInputStream() as InputStream
-                echo is.getText('utf-8')
-                )
+                sh "allure --version"
             }
+//            stage('Telegram notify') {
+//                shell(
+//                def bot_token = "5952779011:AAGHSuT02UfrsFarMFdsO8mKtD1SsqO2EO0"
+//                def chat_id = "1001844362560"
+//
+//                def message = "++++++++ UI tests report ++++++++\n"
+//                message += "Build URL: ${env.JOB_URL}\n"
+//                message += "Report: \n"
+//
+//                def report = readFile "${env.WORKSPACE}/allure-results/export/influxDb.txt"
+//                if (report =~ /.*Failed.*?\s+[1-9]+/) {
+//                    status = " *FAILED*"
+//                }
+//
+//                for (def status : ["Passed", "Failed", "Skipped"]) {
+//                    def matcher = report =~ /.*\$\{status}.*=[1-9]+/
+//                    if (matcher.find()) {
+//                        message += "${status.toUpperCase()}: ${matcher[0][1]}"
+//                    }
+//                }
+//
+//                def url = "https://api.telegram.org/bot${bot_token}/sendMessage?chat_id=-${chat_id}&text=${message}"
+//
+//                if ("${HTTP_PROXY}" != "") {
+//                    def proxyUrl = new URL("${HTTP_PROXY}")
+//                    def proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyUrl.getHost(), proxyUrl.getPort()))
+//                    urlConnection = new URL(url).openConnection(proxy) as HttpURLConnection
+//                } else {
+//                    urlConnection = new URL(url).openConnection() as HttpURLConnection
+//                }
+//
+//                urlConnection.setRequestMethod('GET')
+//                urlConnection.setDoOutput(true)
+//
+//                def is = urlConnection.getInputStream() as InputStream
+//                echo is.getText('utf-8')
+//                )
+//            }
         }
     }
 }
